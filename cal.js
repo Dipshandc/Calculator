e7877b253b44369ff801a849c1a6bf4aeb73094a
Dipshan chhetri <dipshandcnew@gmail.com>
2023-10-08 14:28:12 +0545

    changed

--- cal.js
+++ cal.js
@@ -1,14 +1,10 @@
-//This is va veriables
-//
+// veriables  
 // let calArr =[]
 const calDis = document.getElementById("dis-p")
 const resultDis = document.getElementById("display-p")
 
 function store(a){
 calDis.innerHTML += a
-// if(calArr[0]== undefined){
-//     calArr
-// }
 resultDis.innerHTML = ""
 }
 function startBracket(){
@@ -18,30 +14,6 @@ function startBracket(){
 function closingBracket(){
     calDis.innerHTML += ")"    
     resultDis.innerHTML = ""      
-}
-function forMul(){
-    calDis.innerHTML += "*"
-    resultDis.innerHTML = ""
-}
-function forDiv(){
-    calDis.innerHTML += "/"
-    resultDis.innerHTML = ""
-}
-function forDot(){
-    calDis.innerHTML += "."
-    resultDis.innerHTML = ""
-}
-function forAdd(){
-    calDis.innerHTML += "+"
-    resultDis.innerHTML = ""
-}
-function forSub(){
-    calDis.innerHTML += "-"
-    resultDis.innerHTML = ""
-}
-function forMod(){
-    calDis.innerHTML += "%"
-    resultDis.innerHTML = ""
 }
 function displayResult(){
     result = calDis.innerText
