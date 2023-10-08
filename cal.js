62ab8f238997287b03f13da51c6ae46367195438
Dipshan chhetri <dipshandcnew@gmail.com>
2023-10-08 13:58:44 +0545

    changed from git command

--- cal.js
+++ cal.js
@@ -1,37 +1,46 @@
 // veriables
-let calArr =[]
+// let calArr =[]
 const calDis = document.getElementById("dis-p")
 const resultDis = document.getElementById("display-p")
 
 function store(a){
 calDis.innerHTML += a
-if(calArr[0]== undefined){
-    calArr
-}
+// if(calArr[0]== undefined){
+//     calArr
+// }
+resultDis.innerHTML = ""
 }
 function startBracket(){
     calDis.innerHTML += "("
+    resultDis.innerHTML = ""
 }
 function closingBracket(){
-    calDis.innerHTML += ")"          
+    calDis.innerHTML += ")"    
+    resultDis.innerHTML = ""      
 }
 function forMul(){
     calDis.innerHTML += "*"
+    resultDis.innerHTML = ""
 }
 function forDiv(){
     calDis.innerHTML += "/"
+    resultDis.innerHTML = ""
 }
 function forDot(){
     calDis.innerHTML += "."
+    resultDis.innerHTML = ""
 }
 function forAdd(){
     calDis.innerHTML += "+"
+    resultDis.innerHTML = ""
 }
 function forSub(){
     calDis.innerHTML += "-"
+    resultDis.innerHTML = ""
 }
 function forMod(){
     calDis.innerHTML += "%"
+    resultDis.innerHTML = ""
 }
 function displayResult(){
     result = calDis.innerText
